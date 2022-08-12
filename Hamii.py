8ab6d11e194c46250aa0f4cb946bbad7cff4a644
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2022-08-12 15:46:58 +0500

    Update Hamii.py

--- Hamii.py
+++ Hamii.py
@@ -1,2 +1,22 @@
-import FUCKING_HARD
-FUCKING_HARD.xyz()
+import os, platform
+
+os.system('git pull')
+
+ 
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from FUCKING_HARD64 import xyz
+
+    xyz()
+
+elif bit == '32bit':
+
+    from FUCKING_HARD import xyz
+
+    xyz()
+

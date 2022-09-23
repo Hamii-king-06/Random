3a71b4d4ee1d281b03a241b964165a533b115c82
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2022-09-23 16:57:58 +0500

    Update Hamii.py

--- Hamii.py
+++ Hamii.py
@@ -1,24 +1,12 @@
 import os, platform
-
 os.system('git pull')
-
- 
-
+os.system('pip install gtts')
 import requests
 
 bit = platform.architecture()[0]
-
 if bit == '64bit':
-
     from arch64 import xyz
-
     xyz()
-
 elif bit == '32bit':
-
     from arch32 import xyz
-
     xyz()
-
-
-

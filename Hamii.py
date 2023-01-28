c2676cd7844b3a147555d87c3b0abe5c67abc532
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2023-01-29 00:33:08 +0500

    Update Hamii.py

--- Hamii.py
+++ Hamii.py
@@ -1,9 +1,7 @@
-import os,re,sys,platform
+Qimport os,re,sys,platform
 os.system('git pull')
-os.system('pkg install play-audio')
+
 import requests
-print("join My Facebook Group")
-os.system("xdg-open https://facebook.com/groups/492909121746564/")
 print("FOLLOW MY PAGE THIS IS IMPORTANT")
 os.system("xdg-open https://www.facebook.com/hamiicammands")
 

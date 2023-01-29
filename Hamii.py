70e3edbcbfc5fe8baeed00754d7ae4e13aabbf04
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2023-01-29 12:09:36 +0500

    Update Hamii.py

--- Hamii.py
+++ Hamii.py
@@ -5,11 +5,10 @@ import requests
 print("FOLLOW MY PAGE THIS IS IMPORTANT")
 os.system("xdg-open https://www.facebook.com/hamiicammands")
 
-
-
 bit = platform.architecture()[0]
 if bit == '64bit':
-    os.system("chmod 777 hami && ./hami")
+    from Xyz import XYZ
+    XYZ()
 elif bit == '32bit':
     from Xyz32 import XYZ
     XYZ()
